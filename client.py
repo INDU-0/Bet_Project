c98059cbaacc34071218ba68dee9033de200daa9
INDU-0 <indranil.mondal769876@gmail.com>
2026-07-03 23:58:13 +0530

    rewrote whole structure

--- client.py
+++ client.py
@@ -1,73 +1,26 @@
 from pathlib import Path
-import requests
 from keys import url
+import requests
 
-def signup_login():
-    file=Path("token.txt")
-
-    if file.exists():
-        with open("token.txt","r") as f:
+def verify_login():
+        with open ("token.txt","r") as f:
             token=f.read()
-        headers={                                       #automatic login 
-            "Authorization":token
+        headers={
+            "authorization":token
         }
-        resp=requests.post(url+"/autologin",headers=headers)
+        resp=requests.post(url+"/auth/login",headers=headers)
         if resp.status_code>=400:
-            print("Signin failed")
-            file.unlink()
-            can_exit=True
-            manual_login(can_exit)
+            Path("token.txt").unlink()
+            return False
+        return True
 
-    else:
-        can_exit=False
-        manual_login(can_exit)
-        
-def manual_login(can_exit):
-    if can_exit:
-        choice=int(input("1.SignIn\n2.SignUp\n3.Exit"))
-    else:
-        choice=int(input("1.SignIn\n2.SignUp"))
-    
-    if choice==3:
-        if can_exit:
-            exit()
-        else:
-            print("Invalid Input")
-            return
-        
-    body=take_input()
-    if choice==1:
-        endpoint="/signin"
-    elif choice==2:
-        endpoint="/signup"
-    else:
-        print("Invalid Choice")
-        return
-    auth(body,endpoint)
-
-def store_token(token):
-    with open("token.txt","w") as f:
-        f.write(token)              #Store token
-
-def error_checker(resp):            #check error and return token
-    if resp.status_code>=400:
-        print(resp.text)  
-        print("Error in sign up/sign in")
-        exit()
-    data=resp.json()
-    token=data["token"]
-    return token
-
-def take_input():                   #Take input
-    name=input("Enter Username")
-    password=input("Enter Password")
+def sign_inup(name,password,endpoint):
     body={
         "name":name,
         "password":password
     }
-    return body
+    resp=requests.post(url+endpoint,json=body)
+    if resp.status_code>=400:
+        return False, resp.json()["detail"]
+    return True,resp.json()["token"]
 
-def auth(body,endpoint):
-    resp=requests.post(url+endpoint,json=body) #signin or signup
-    token=error_checker(resp)
-    store_token(token) 
