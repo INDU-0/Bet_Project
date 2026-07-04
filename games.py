6358710823024c0a50145a29abdf62f42b55e2db
INDU-0 <indranil.mondal769876@gmail.com>
2026-07-04 21:02:49 +0530

    added high low

--- games.py
+++ games.py
@@ -10,29 +10,45 @@ def coinflip(balance,bet,user_ht:int): #balance is bet minus balance
         return False, balance, head_tail
 
 def blackjack():
-    deck = []
-    suits = ["♠", "♥", "♦", "♣"]
-    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    
-    for suit in suits:
-        for rank in ranks:
-            deck.append(f"{rank}{suit}")
-    random.shuffle(deck)
-
-    dealer_hand=[]
-    player_hand=[]
-    for r in range(2):
-        dealer_hand.append(deck.pop())
-        player_hand.append(deck.pop())
-
-    #we gotta show dealer 1 and give the player both of his cards
-    
-
-def high_low():
     pass
+    
+def high_low(bet,ch,comp1):
+    low = max(1, comp1 - 3)
+    high = min(15, comp1 + 3)
+    comp2=random.randint(low,high)
+
+    if comp1==comp2:
+        bet=bet*10
+    if ch==1 and comp1<comp2:
+        bet=bet*(comp2-comp1)
+        state=True
+    elif ch==1 and comp1>comp2:
+        bet=bet*(comp2-comp1)
+        state=False
+    elif ch==2 and comp1>comp2:
+        bet=bet*(comp1-comp2)
+        state=True
+    elif ch==2 and comp1<comp2:
+        bet=bet*(comp2-comp1)
+        state=False
+
+    return bet,state,comp2
 
 def slots():
     pass
 
 def mines():
+    pass
+
+# def deck_maker():
+#     deck = []
+#     suits = ["♠", "♥", "♦", "♣"]
+#     ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
+#     for suit in suits:
+#         for rank in ranks:
+#             deck.append(f"{rank}{suit}")
+#     return deck
+
+def card_value(card):
     pass
